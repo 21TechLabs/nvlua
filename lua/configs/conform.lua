048c0e1878776e0424857253f9f8dca4422ae795
Abhay Singh <abhay1029singh@gmail.com>
2025-06-16 00:28:40 +0530

    added important lsps

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,20 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    javascript = { "biome", "biome-organize-imports" },
+    javascriptreact = { "biome", "biome-organize-imports" },
+    typescript = { "biome", "biome-organize-imports" },
+    typescriptreact = { "biome", "biome-organize-imports" },
+    go = { "gofumpt", "goimports-reviser", "golines" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,74 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "svelte" }
 vim.lsp.enable(servers)
 
+local lspconfig = require("lspconfig")
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local capabilities = require("nvchad.configs.lspconfig").capabilities
 -- read :h vim.lsp.config for changing options of lsp servers 
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "gopls" },
+  filetypes = {"go", "gomod", "gowork", "gotmpl"},
+  root_dir = {"go.work", "go.mod", "main.go"},
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      completeUnimported = true,
+      usePlaceholders = true,
+      staticcheck = true,
+    },
+  },
+}
+
+lspconfig.svelte.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "svelteserver", "--stdio" },
+  filetypes = { "svelte" },
+  root_dir = {"package.json", "tsconfig.json", ".git", "svelte.config.json"},
+}
+
+lspconfig.biome.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "biome", "lsp" },
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte" },
+  root_dir = {"package.json", "tsconfig.json", ".git"},
+}
+
+lspconfig.pylsp.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "pylsp" },
+  filetypes = { "python" },
+  root_dir = {"setup.py", "pyproject.toml", "tox.ini", ".git", "main.py", "app.py", "requirements.txt",},
+  settings = {
+    pylsp = {
+      plugins = {
+        pyflakes = { enabled = true },
+        pycodestyle = { enabled = true, maxLineLength = 79 },
+        black = { enabled = true, lineLength = 79 },
+        isort = { enabled = true, profile = "black" },
+      },
+    },
+  },
+}
+
+lspconfig.angularls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "ngserver", "--stdio" },
+  filetypes = { "typescript", "html" },
+  root_dir = {"angular.json", "workspace.json", ".git"},
+  settings = {
+    angular = {
+      strictTemplates = true,
+    },
+  },
+}
